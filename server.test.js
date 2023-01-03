317709bf626b5dbf27e33a74eb4d82d247f39fe2
KrazyGearz <bt-mcquitter@wiu.edu>
2023-01-02 19:38:36 -0600

    full testing for adding a book and returning a book by ID

--- server.test.js
+++ server.test.js
@@ -64,47 +64,51 @@ describe('api', () =>
       });
     });
     describe('add book', () => {
-        it('should return a book by ID', async () => {
+
+      const addbook = `
+      mutation AddBook($title: String!, $authorId: ID!, $categoryIds: [ID!]!, $addBookDescription2: String) {
+        addBook(title: $title, authorId: $authorId, categoryIds: $categoryIds, description: $addBookDescription2) {
+          title
+          id
+          description
+          categories {
+            id
+          }
+          author {
+            id
+          }
+        }
+      }
+          `;
+        it('should add a book and return added info', async () => {
             const response = await testServer.executeOperation(
                 {
-                  query: getbook,
-                  variables: { getBookId: 2 },
+                  query: addbook,
+                  variables: { addBookDescription2: "This is a description",
+                  title: "Some great book",
+                  authorId: 2,
+                  categoryIds: [1,2] },
                 });
             expect(response.body.singleResult.data).toEqual(
                 {
-                    "getBook": {
-                        "title": "Harry Potter and the Prisoner of Azkaban",
-                        "id": "2",
-                        "description": null,
-                        "coverImage": "https://m.media-amazon.com/images/I/51DQeuJ5QDL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
-                        "categories": [
-                          {
-                            "name": "Fantasy",
-                            "id": "1"
-                          },
-                          {
-                            "name": "Fiction",
-                            "id": "2"
-                          }
-                        ]
+                  "addBook": {
+                    "title": "Some great book",
+                    "id": "5",
+                    "description": "This is a description",
+                    "categories": [
+                      {
+                        "id": "1"
                       },
+                      {
+                        "id": "2"
+                      }
+                    ],
+                    "author": {
+                      "id": "2"
+                    }
+                  },
                 },
               );
           });
-      
-      it('should with the correct pet if it exists', async () => {
-        const response = await request(app).get('/api/v1/pets/1');
-        expect(response.body).toEqual(pets[0]);
-      });
-
-      it('should return status 404 if the pet does not exists', async () => {
-        const response = await request(app).get('/api/v1/pets/7');
-        expect(response.status).toBe(404);
-      });//
-      
-      it('should with respond Pet not found', async () => {
-        const response = await request(app).get('/api/v1/pets/7');
-        expect(response.body).toEqual('Pet not found');
-      });
     })
   });
